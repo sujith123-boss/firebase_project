18a882b42b9483f28163f9d2c6c14fc4812a1e78
SujithTechSavy <56557421+sujith123-boss@users.noreply.github.com>
2024-06-17 18:36:09 +0530

    Create app.py

---
+++ poc_app/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+from flask_restful import Api
+import firebase_admin
+from firebase_admin import credentials
+from api.resources import ProfileResource, ProfileListResource
+
+app = Flask(__name__)
+api = Api(app)
+
+cred = credentials.Certificate('path_to_your_service_account_key.json')
+firebase_admin.initialize_app(cred)
+
+api.add_resource(ProfileListResource, '/api/profiles')
+api.add_resource(ProfileResource, '/api/profiles/<string:profile_id>')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
