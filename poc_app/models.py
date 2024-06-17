d7d3e9f7d6162c4f08cb5236d6c062ef92bb52a9
SujithTechSavy <56557421+sujith123-boss@users.noreply.github.com>
2024-06-17 18:45:38 +0530

    Create models.py

---
+++ poc_app/models.py
@@ -0,0 +1,10 @@
+profiles = {}
+
+class Profile:
+    def __init__(self, user_id, name, email):
+        self.user_id = user_id
+        self.name = name
+        self.email = email
+
+    def to_dict(self):
+        return {'user_id': self.user_id, 'name': self.name, 'email': self.email}
